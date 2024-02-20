1d844113d5f1227efbf0a87b0aa1e772dc7c7760
hayoung Park <mcd158@naver.com>
2024-02-20 15:17:44 +0900

    주소 수정

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_KEY = `452a4ea137fa426281779a008fa281d0`;
 let news = [];//여러번 쓰일 것이므로 전역변수로 할당해주는 것임
 //https://newsapi.org/v2/top-headlines
 const getLatestNews = async () => {
-	const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?country=us&apiKey=${API_KEY}`);
+	const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
 
 	const response = await fetch(url);//await을 포함하는 것은 비동기 함수이다.
 	const data = await response.json();//객체 형식으로 주고받기 편하므로 json을 사용
