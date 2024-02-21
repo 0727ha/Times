4cf79031dadf3f8aba424e7a30b7bbe158513b22
hayoung Park <mcd158@naver.com>
2024-02-22 01:37:43 +0900

    UI 수정 필요

--- main.js
+++ main.js
@@ -29,3 +29,22 @@ const render = () => {
 };
 
 getLatestNews();
+
+const openNav = () => {
+	document.getElementById("mySidenav").style.width = "250px";
+};
+openNav();
+const closeNav = () => {
+	document.getElementById("mySidenav").style.width = "0";
+};
+closeNav();
+
+const openSearchBox = () => {
+	let inputArea = document.getElementById("input-area");
+	if (inputArea.style.display === "inline") {
+		inputArea.style.display = "none";
+	} else {
+		inputArea.style.display = "inline";
+	}
+};
+openSearchBox();
