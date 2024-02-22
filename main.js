806abb0feccbab3f95e0ea359e44c1c38aa3a326
hayoung Park <mcd158@naver.com>
2024-02-23 01:48:43 +0900

    제출용url로 수정

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const API_KEY = `452a4ea137fa426281779a008fa281d0`;
 let newsList = [];//여러번 쓰일 것이므로 전역변수로 할당해주는 것임
 const menus = document.querySelectorAll(".menus button");
 menus.forEach((menu) => menu.addEventListener("click", (Event) => getNewsByCategory(Event)));
-//https://newsapi.org/v2/top-headlines
+//https://newsapi.org/v2/top-headlines(제출용)
 //https://timesnews-site.netlify.app//top-headlines
 const getLatestNews = async () => {
-	const url = new URL(`https://timesnews-site.netlify.app//top-headlines?country=us&apiKey=${API_KEY}`);
+	const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
 
 	const response = await fetch(url);//await을 포함하는 것은 비동기 함수이다.
 	const data = await response.json();//객체 형식으로 주고받기 편하므로 json을 사용
