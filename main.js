3d03921db54b087ad2668eaa09dd28a71e700228
hayoung Park <mcd158@naver.com>
2024-02-25 01:42:11 +0900

    pagination

--- main.js
+++ main.js
@@ -3,10 +3,17 @@ let newsList = [];//여러번 쓰일 것이므로 전역변수로 할당해주�
 const menus = document.querySelectorAll(".menus button");
 menus.forEach((menu) => menu.addEventListener("click", (Event) => getNewsByCategory(Event)));
 
-let url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+let url = new URL(`https://timesnews-site.netlify.app//top-headlines?country=us&apiKey=${API_KEY}`);
+let totalResults = 0;
+let page = 1;
+const pageSize = 10;
+const groupSize = 5;
 
 const getNews = async () => {
 	try {
+		url.searchParams.set("page", page);//=> &page=page
+		url.searchParams.set("pageSize", pageSize);
+
 		const response = await fetch(url);//await을 포함하는 것은 비동기 함수이다.
 
 		const data = await response.json();//객체 형식으로 주고받기 편하므로 json을 사용
@@ -15,7 +22,9 @@ const getNews = async () => {
 				throw new Error("no result for this search");
 			}
 			newsList = data.articles;
+			totalResults = data.totalResults;
 			render();
+			paginationRender();
 		} else {
 			throw new Error(data.message);
 		}
@@ -78,6 +87,52 @@ const errorRender = (errorMessage) => {
 	      </div>`;
 
 	document.getElementById("news-board").innerHTML = errorHTML;
+};
+
+const paginationRender = () => {
+	//totalresult
+	//page
+	//pagesize
+	//groupsize
+	//totalPages
+	const totalPages = Math.ceil(totalResults / pageSize);
+
+	//pageGroup
+	const pageGroup = Math.ceil(page / groupSize);
+	//lastPage
+	const lastPage = pageGroup * groupSize;
+	//마지막 페이지 그룹이 그룹사이즈보다 작다? lastpage=totalpage
+	if (lastPage > totalPages) {
+		lastPage = totalPages;
+	}
+	//firstPage
+	const firstPage = lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
+
+	let paginationHTML = ``;
+
+	for (let i = firstPage; i <= lastPage; i++) {
+		paginationHTML += `<li class="page-item ${i === page ? `active` : ``}" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`;
+	}
+	document.querySelector(".pagination").innerHTML = paginationHTML;
+
+
+
+	/*<nav aria-label="Page navigation example">
+	  <ul class="pagination">
+		<li class="page-item"><a class="page-link" href="#">Previous</a></li>
+		<li class="page-item"><a class="page-link" href="#">1</a></li>
+		<li class="page-item"><a class="page-link" href="#">2</a></li>
+		<li class="page-item"><a class="page-link" href="#">3</a></li>
+		<li class="page-item"><a class="page-link" href="#">Next</a></li>
+	  </ul>
+	</nav>*/
+
+};
+
+const moveToPage = (pageNum) => {
+	console.log("movetoPage", pageNum);
+	page = pageNum;
+	getNews()
 };
 
 getLatestNews();
